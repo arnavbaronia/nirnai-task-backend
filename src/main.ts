4c5f75fdcc0eacbedeaf14debc4b2fe4d2d48cb2
arnavbaronia <arnav.baronia@gmail.com>
2025-05-10 17:53:33 +0530

    cors added

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   // Increase payload limit
+  app.enableCors({
+    origin: 'http://localhost:3000', // Your frontend URL
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   
